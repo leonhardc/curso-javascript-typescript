04ce262f46acb07742418182840c89db0a6ffeb9
leonardo <leonardo99692370costa@gmail.com>
2026-04-20 13:34:43 -0300

    Arquivos da secao 09 do curso de javascript

---
+++ Secao 9 - JavaScript Tooling e ES6 Modules - Modulos/aula118/public/assets/js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js"
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+() {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: [BABEL] /home/leonardo/Área de Trabalho/curso-javascript-typescript/Secao 9 - JavaScript Tooling e ES6 Modules - Modulos/aula118/src/index.js: Using removed Babel 5 option: .modules - Use the corresponding module transform plugin in the `plugins` option. Check out http://babeljs.io/docs/plugins/#modules");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Secao 9 - JavaScript Tooling e ES6 Modules - Modulos/aula118/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Leonardo'
+const sobrenome =  'Rodrigues'
+const idade = '30'
+
+function soma(x, y){
+    return x + y
+}
+
+export { nome, sobrenome, idade, soma }
