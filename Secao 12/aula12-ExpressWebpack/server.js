48d74749543b5cc7c7507d27e0df4791a3152d61
Leonardo <leonardo99692370costa@gmail.com>
2026-07-08 13:35:22 -0300

    Aula 12 aula sobre Express e Webpack

---
+++ Secao 12/aula12-ExpressWebpack/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+// definindo a pasta de views e o motor de template
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
