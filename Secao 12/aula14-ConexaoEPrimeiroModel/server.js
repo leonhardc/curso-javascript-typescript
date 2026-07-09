439a68c68d771207a7d036795069f6719566c27f
Leonardo <leonardo99692370costa@gmail.com>
2026-07-09 11:21:19 -0300

    Aula 14 sobre mongoDB

---
+++ Secao 12/aula14-ConexaoEPrimeiroModel/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso!');
+    // Se a conexão for bem-sucedida, emite o evento 'pronto'
+    app.emit('pronto');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  });
+
+const routes = require('./routes');
+const path = require('path');
+const meuMiddleware = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+// definindo a pasta de views e o motor de template
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(routes);
+
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
+});
---
+++ Secao 12/aula14-ConexaoEPrimeiroModel/src/controllers/homeController.js
@@ -0,0 +1,20 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+  title: 'Título de teste',
+  description: 'Descrição de teste'
+})
+  .then((dados) => {
+    console.log('Documento criado com sucesso!', dados);
+  })
+  .catch((error) => {
+    console.error('Erro ao criar o documento:', error);
+  }); 
+
+exports.paginaInicial = (req, res) => {
+  res.render('index')
+};
+
+exports.trataPost = (req, res) => {
+  res.send('Recebi o formulário');
+};
