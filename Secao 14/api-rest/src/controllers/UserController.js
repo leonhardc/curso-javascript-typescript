3e93f9e355df20a8c97ad521f0f182a939053864
Leonardo <leonardo99692370costa@gmail.com>
2026-08-08 17:05:18 -0300

    Finalizado CRUD de usuarios na API - aula a76

--- Secao 14/api-rest/src/controllers/UserController.js
+++ Secao 14/api-rest/src/controllers/UserController.js
@@ -5,11 +5,83 @@ class UserController {
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json({
+      return res.json({
         novoUser,
       });
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      return res.json(users);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Show
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["O usuario não existe"],
+        })
+      }
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Update
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["O usuario não existe"],
+        })
+      }
+      const userUpdated = await user.update(req.body);
+      return res.json(userUpdated);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["O usuario não existe"],
+        });
+      }
+      await user.destroy();
+      return res.json({ message: "Usuario deletado com sucesso" });
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- Secao 14/api-rest/src/routes/userRoutes.js
+++ Secao 14/api-rest/src/routes/userRoutes.js
@@ -4,7 +4,10 @@ import userController from "../controllers/UserController.js";
 const router = new Router();
 
 router.post('/', userController.create);
-
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 /*
   index: lista todos os usuarios - GET
   store/create: cria um novo usuario - POST
